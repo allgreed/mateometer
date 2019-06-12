94fbebc75963fd65da7d075dd289b1db98b99166
Olgierd "Allgreed" Kasprowicz <olixem@gmail.com>
2019-06-13 00:26:37 +0200

    Get rid of unsafe in favour of Rocket state

--- src/main.rs
+++ src/main.rs
@@ -1,92 +1,100 @@
 #![feature(proc_macro_hygiene, decl_macro)]
 #[macro_use] extern crate rocket;
+
+use std::sync::atomic::{AtomicU16, Ordering};
+
 use prometheus::{Opts, Registry, Gauge, TextEncoder, Encoder};
+use rocket::State;
 
 
-// TODO: Fix unsafe
-// TODO: Fix metrics - use global registry xd
+// TODO: See Trello - metric
 // TODO: Add persistance
+
 // TODO: Add admin action authentication
 // TODO: JSON support
 // TODO: start FROM scratch and copy the required linked binaries / link statically
 // TODO: Add swagger contract
 // TODO: Add DMZ support
 // TODO: Don't use GET (for adding, setting and taking one) - GETs should be idempotent
-
-
-static mut ble: i32 = 0;
-
-
 #[get("/count")]
-fn get_mate() -> String {
-    unsafe {
-       format!("{}", ble)
-    }
+fn get_mate(mate_count: State<AtomicU16>) -> String {
+    mate_count.load(Ordering::Relaxed).to_string()
 }
 
 // TODO: Can I alias it somehow?
 #[get("/")]
-fn root() -> String {
-    unsafe {
-       format!("{}", ble)
-    }
+fn root(mate_count: State<AtomicU16>) -> String {
+    mate_count.load(Ordering::Relaxed).to_string()
 }
 
 #[get("/add/<amount>")]
-fn add_mate(amount: i32) -> String {
-    let dddd = alter_mate_amount(amount);
+fn add_mate(mate_count: State<AtomicU16>, amount: u16) -> String {
+    let current_mate_count = mate_count.load(Ordering::Relaxed);
 
-    format!("{}", dddd)
+    let new_mate_count = current_mate_count + amount;
+
+    mate_count.store(new_mate_count, Ordering::Relaxed);
+
+    new_mate_count.to_string()
 }
 
 #[get("/set/<amount>")]
-fn set_mate(amount: i32) -> String {
-    let dddd = alter_mate_amount(- alter_mate_amount(0) + amount);
+fn set_mate(mate_count: State<AtomicU16>, amount: u16) -> String {
+    mate_count.store(amount, Ordering::Relaxed);
 
-    format!("{}", dddd)
+    amount.to_string()
 }
 
 #[get("/one")]
-fn remove_single_mate() -> &'static str {
-    alter_mate_amount(-1);
+fn remove_single_mate(mate_count: State<AtomicU16>) -> &'static str {
+    let current_mate_count = mate_count.load(Ordering::Relaxed);
+
+    let new_mate_count = current_mate_count - 1;
+
+    mate_count.store(new_mate_count, Ordering::Relaxed);
 
     "Smacznego! Możesz zamknąć tę stronę :)"
 }
 
-fn alter_mate_amount(fuj: i32) -> i32 {
-    unsafe {
-        ble += fuj;
-        ble
-    }
-}
-
 #[get("/metrics")]
-fn get_metrics() -> String {
-    let r = Registry::new();
-    let counter_opts = Opts::new("mateometer_mate_count", "amount of mate in our Hackerspace");
-    let counter = Gauge::with_opts(counter_opts).unwrap();
-
-    unsafe {
-        counter.set(ble.into());
-    }
-
-    r.register(Box::new(counter.clone())).unwrap();
+fn get_metrics(mate_gauge: State<Gauge>, metrics_registry: State<Registry>, mate_count: State<AtomicU16>) -> String {
+    mate_gauge.set(mate_count.load(Ordering::Relaxed).into());
 
     let mut buffer = vec![];
-    let encoder = TextEncoder::new();
-    let metric_families = r.gather();
-    encoder.encode(&metric_families, &mut buffer).unwrap();
 
-    String::from_utf8(buffer).unwrap()
+    TextEncoder::new()
+        .encode(&metrics_registry.gather(), &mut buffer)
+        .unwrap();
+
+    String::from_utf8(buffer)
+        .unwrap()
 }
 
 fn main() {
-    rocket::ignite().mount("/", routes![
-        root,
-        get_mate,
-        get_metrics,
-        add_mate,
-        set_mate,
-        remove_single_mate,
-    ]).launch();
+    let mate_count = AtomicU16::new(0);
+
+    // TODO: Less painfull to extract matecount - type with some impls
+    // TODO: Type for passing metrics around
+    let mate_gauge = Gauge::with_opts(Opts::new("mateometer_mate_count", "amount of mate in our Hackerspace"))
+        .unwrap();
+
+    let metrics_registry = Registry::new();
+
+    metrics_registry.register(Box::new(mate_gauge.clone()))
+        .unwrap();
+
+    rocket::ignite()
+        .manage(metrics_registry)
+        .manage(mate_gauge)
+        .manage(mate_count)
+        .mount("/", routes![
+            root,
+            get_mate,
+            get_metrics,
+            add_mate,
+            set_mate,
+            remove_single_mate,
+        ]
+    )
+    .launch();
 }
